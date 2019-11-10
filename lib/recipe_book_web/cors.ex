65f2bfa7ebbc6fff77c08395b8f3520ef7f4397b
bees <arian.daneshvar@gmail.com>
2019-11-09 19:06:15 -0600

    remove unused CORS plug

--- lib/recipe_book_web/cors.ex
+++
@@ -1,8 +0,0 @@
-defmodule RecipeBookWeb.CORS do
-  use Corsica.Router,
-    origins: ["http://localhost:3000", ~r{^https?://(.*\.?)foo\.com$}],
-    allow_credentials: true,
-    max_age: 600
-
-  resource("/*", origins: "*")
-end
